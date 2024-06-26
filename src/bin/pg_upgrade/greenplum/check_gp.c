1b2ce0332241fc6ddfe53dcd435977c78707dcc1
Kevin Yeap <kyeap@vmware.com>
2024-06-26 14:09:24 -0700

    pg_upgrade 6 > 7: check for views using removed tables or views
    Uses a 6x support functions to check to views that use removed tables,
    views, and materialized views. The following tables and views have been
    removed in 7x. User views with references to removed tables or views
    cannot exist or pg_restore will fail during upgrade.
    
    Steps to get the list of removed tables, views, and materialized views.
    
    on 7x template1
    ```
    COPY (SELECT relname FROM pg_class) TO '/tmp/7x_relname.csv' WITH CSV;
    (COPY 626)
    ```
    
    on 6X template1
    ```
    CREATE TEMP TABLE seven_relname(a text);
    COPY seven_relname FROM '/tmp/7x_relname.csv';
    
    CREATE TEMP TABLE diff AS (
      SELECT relname FROM pg_class
      EXCEPT
      SELECT * FROM seven_relname
    );
    (SELECT 43)
    
    SELECT c.oid , n.nspname || '.' || c.relname
    FROM pg_class c
    JOIN pg_namespace n ON c.relnamespace = n.oid
    JOIN diff d ON d.relname = c.relname
    WHERE relkind IN ('r', 'v', 'm')
    AND c.oid < 16384
    ORDER BY oid;
    
      oid  |                     ?column?
    -------+--------------------------------------------------
      5010 | pg_catalog.pg_partition
      5011 | pg_catalog.pg_partition_rule
      9903 | pg_catalog.pg_partition_encoding
     11782 | pg_catalog.pg_partitions
     11786 | pg_catalog.pg_partition_columns
     11789 | pg_catalog.pg_partition_templates
     11796 | pg_catalog.pg_stat_partition_operations
     12365 | gp_toolkit.__gp_user_data_tables
     12474 | gp_toolkit.gp_size_of_partition_and_indexes_disk
    (9 rows)
    ```
    
    6x support functions commit reference:
    https://github.com/greenplum-db/gpdb/commit/3ff5c142dfc55a6f0ee314c3e0964a2ad4e8c458
    
    Co-authored-by: Divyesh Vanjare <vanjared@vmware.com>

--- src/bin/pg_upgrade/greenplum/check_gp.c
+++ src/bin/pg_upgrade/greenplum/check_gp.c
@@ -34,6 +34,7 @@ static void check_for_disallowed_pg_operator(void);
 static void check_for_ao_matview_with_relfrozenxid(ClusterInfo *cluster);
 static void check_views_with_changed_function_signatures(void);
 static void check_views_with_removed_columns(void);
+static void check_views_with_removed_tables(void);
 
 /*
  *	check_greenplum
@@ -61,6 +62,7 @@ check_greenplum(void)
 	check_views_with_changed_function_signatures();
 	check_views_with_removed_columns();
 	check_for_ao_matview_with_relfrozenxid(&old_cluster);
+	check_views_with_removed_tables();
 }
 
 /*
@@ -1440,6 +1442,95 @@ check_views_with_removed_columns()
 			"| Your installation contains views using\n"
 			"| columns from catalog tables that have been removed.\n"
 			"| These views must be updated to use columns supported in the\n"
+			"| target version or removed before upgrade can continue. A list\n"
+			"| of the problem views is in the file:\n\t%s\n\n", output_path);
+	}
+	else
+		check_ok();
+}
+
+/* checks for views with removed tables, views, and materialized views */
+static void
+check_views_with_removed_tables()
+{
+	if (GET_MAJOR_VERSION(old_cluster.major_version) > 904)
+		return;
+
+	char  output_path[MAXPGPATH];
+	FILE *script = NULL;
+	int   dbnum;
+	int   i_viewname;
+
+	prep_status("Checking for views with removed tables");
+
+	snprintf(output_path, sizeof(output_path), "%s/%s",
+			 log_opts.basedir, "views_with_removed_tables.txt");
+
+	for (dbnum = 0; dbnum < old_cluster.dbarr.ndbs; dbnum++)
+	{
+		PGresult   *res;
+		int			ntups;
+		int			rowno;
+		DbInfo	   *active_db = &old_cluster.dbarr.dbs[dbnum];
+		PGconn	   *conn;
+		bool		db_used = false;
+
+		conn = connectToServer(&old_cluster, active_db->db_name);
+		PQclear(executeQueryOrDie(conn, "SET search_path TO 'public';"));
+
+		/*
+		 * Disabling track_counts results in a large performance improvement of
+		 * several orders of magnitude when walking the views. This is because
+		 * calling try_relation_open to get a handle of the view calls
+		 * pgstat_initstats which has been profiled to be very expensive. For
+		 * our purposes, this is not needed and disabled for performance.
+		 */
+		PQclear(executeQueryOrDie(conn, "SET track_counts TO off;"));
+
+		/* Install check support function */
+		PQclear(executeQueryOrDie(conn,
+								  "CREATE OR REPLACE FUNCTION "
+								  "view_has_removed_tables(OID) "
+								  "RETURNS BOOL "
+								  "AS '$libdir/pg_upgrade_support' "
+								  "LANGUAGE C STRICT;"));
+		res = executeQueryOrDie(conn,
+								"SELECT quote_ident(n.nspname) || '.' || quote_ident(c.relname) AS badviewname "
+								"FROM pg_class c JOIN pg_namespace n on c.relnamespace=n.oid "
+								"WHERE c.relkind = 'v' "
+								"AND c.oid >= 16384 "
+								"AND view_has_removed_tables(c.oid) = TRUE;");
+
+		PQclear(executeQueryOrDie(conn, "DROP FUNCTION view_has_removed_tables(OID);"));
+		PQclear(executeQueryOrDie(conn, "SET search_path to 'pg_catalog';"));
+		PQclear(executeQueryOrDie(conn, "RESET track_counts;"));
+
+		ntups = PQntuples(res);
+		i_viewname = PQfnumber(res, "badviewname");
+		for (rowno = 0; rowno < ntups; rowno++)
+		{
+			if (script == NULL && (script = fopen(output_path, "w")) == NULL)
+				pg_fatal("Could not create necessary file:  %s\n", output_path);
+			if (!db_used)
+			{
+				fprintf(script, "In database: %s\n", active_db->db_name);
+				db_used = true;
+			}
+			fprintf(script, "  %s\n", PQgetvalue(res, rowno, i_viewname));
+		}
+
+		PQclear(res);
+		PQfinish(conn);
+	}
+
+	if (script)
+	{
+		fclose(script);
+		pg_log(PG_REPORT, "fatal\n");
+		gp_fatal_log(
+			"| Your installation contains views using\n"
+			"| catalog tables that have been removed.\n"
+			"| These views must be updated to use tables supported in the\n"
 			"| target version or removed before upgrade can continue. A list\n"
 			"| of the problem views is in the file:\n\t%s\n\n", output_path);
 	}
